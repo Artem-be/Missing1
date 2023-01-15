8cb9962740c82bef5c8f08f66e65606e1ffc2f86
Artem-be <111177225+Artem-be@users.noreply.github.com>
2023-01-15 08:30:12 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,28 +1,28 @@
 import unittest
-import  zadanie1
+import zadanie1
 from zadanie1 import offset
 
 
-class TestSum(unittest.TestCase):
+class Test(unittest.TestCase):
     def setUp(self):
         self.number = offset
 
-    def test_sum1(self):
+    def test1(self):
         self.assertTrue(type(self.number) is int)
-    def test_sum2(self):
+    def test2(self):
         self.assertTrue(type(self.number) is int)
-    def test_sum3(self):
+    def test3(self):
         self.assertFalse(type(self.number) is float)
-    def test_sum4(self):
+    def test4(self):
         self.assertFalse(type(self.number) is str)
 
 
 
     if __name__ == '__main__':
 
-        test_sum1()
-        test_sum2()
-        test_sum3()
-        test_sum4()
+        test1()
+        test2()
+        test3()
+        test4()
 
         print("Everything passed")
